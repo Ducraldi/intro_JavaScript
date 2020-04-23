d08ef0a25770c01494295a47bf1ae99be77dadb8
Ducraldi <ducraldi@gmail.com>
2020-04-23 16:23:27 -0400

    Reglas basicas enjS, variables, constantes y tipos de datos primitivos.

---
+++ 01_variables/tipo_datos.js
@@ -0,0 +1,32 @@
+// Texto = String
+let nombreCompleto = "Ducraldi";
+let apellido = 'Fabian';
+let pais = 'Republica Dominicana'
+
+// Numero = Number
+let edad = 23;
+let year = 2020;
+let millon = 1000000;
+const PI - 3.14;
+let precioDollar =54.8;
+
+// Logicos = Boolean
+let tieneCarro = false;
+let posseePC = true;
+
+console.log(nombreCompleto);
+console.log(edad);
+console.log(tieneCarro);
+
+console.log(typeof nombreCompleto);
+console.log(typeof edad);
+console.log(typeof tieneCarro);
+
+let precio = 10;
+
+console.log(typeof precio);
+
+let resultado = precio + 15;
+
+console.log(resultado);
+
---
+++ 01_variables/variables.js
@@ -0,0 +1,17 @@
+"user strict"
+
+// nombre completo 
+ 
+let nombreCompleto = 'Ducraldi';
+
+// pais de origen
+
+let paisDeOrigen = 'Rep, Dom';
+
+// fecha de nacimiento
+
+let fechaDeNacimiento = '24/5/2000';
+
+// balance inicial para abrir la cuenta
+
+let balanceInicialParaAbrirLaCuenta =  1000;
