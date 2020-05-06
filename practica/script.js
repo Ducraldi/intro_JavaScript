e6d88ed093b92f034ff473e50766dc3aec7909d5
Ducraldi <Ducraldi@users.noreply.github.com>
2020-05-06 16:40:55 -0400

    ocurtando h1 y poniendo estilos.

---
+++ practica/script.js
@@ -0,0 +1,14 @@
+// Aqui selecionamos las variables con querySelector
+let btnFuncion = document.querySelector("#funcion");
+let bienvenidoPagina = document.querySelector("#bienvenidoPagina");
+let nuevoTitulo = document.querySelector("#nuevoTitulo");
+
+// aqui añadimos los evento que pasaran al precionar el boton 
+btnFuncion = addEventListener("click", function(){
+
+    bienvenidoPagina.style.display = "none"; // añadimos stylo None para ocurtal
+    nuevoTitulo.innerHTML = ("<i>Haz hecho doble click</i>"); // añadimos el comentario y  la cursiva
+    nuevoTitulo.style.color = "red"; // aqui añadimos el color alalectras 
+});
+
+console.log(btnFuncion);
